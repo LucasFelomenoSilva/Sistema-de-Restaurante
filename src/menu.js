4d6db044d08074f8423b39c31de654e75ff2e1dc
jorgenet0h <137846921+jorgenet0h@users.noreply.github.com>
2025-10-09 12:13:02 -0300

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,25 +1,21 @@
-import readlineSync from "readline-sync"; // Biblioteca para ler entradas do usuário no terminal
-import Cliente from './cliente.js';      // Importa a classe Cliente
-import Pedido from './pedido.js';        // Importa a classe Pedido
-
+import readlineSync from "readline-sync";
+import Cliente from './cliente.js';
+import Pedido from './pedido.js';
 // Criando instâncias das classes
 const cliente = new Cliente();
 const pedido = new Pedido();
 
 // Função principal que exibe o menu e processa as escolhas
 export function iniciarMenu() {
-    while (true) { // Loop infinito: continuará até o usuário escolher sair
-        // Exibe o menu
+    while (true) {
         console.log("\n|1 - Cadastrar Cliente =-=|");
         console.log("|2 - Listar Cliente =-=-=-|");
         console.log("|3 - Criar Pedido =-=-=-=-|");
         console.log("|4 - Listar Pedido =-=-=-=|");
         console.log("|5 - Sair =-=-=-=-=-=-=-=-|");
 
-        // Solicita a escolha do usuário
         const resposta = readlineSync.question("Escolha uma opcao: ");
 
-        // Verifica a opção escolhida e chama o método correspondente
         switch (resposta) {
             case "1":
                 cliente.cadastrar(); // Chama o método de cadastro do cliente
@@ -34,7 +30,7 @@ export function iniciarMenu() {
                 pedido.listar();     // Chama o método para listar pedidos
                 break;
             case "5":
-                console.log("\nEncerrando programa..."); // Mensagem de saída
+                console.log("\nEncerrando programa...");
                 return; // Sai do loop e encerra a função
             default:
                 console.log("\nOpcao invalida!"); // Caso o usuário digite algo errado
@@ -42,5 +38,4 @@ export function iniciarMenu() {
     }
 }
 
-// Chama a função para iniciar o menu assim que o arquivo for executado
 iniciarMenu();""
